105fe6b664b98f4e8ade346f9abdefa9c476a066
Bayu Arma Praja <100572149+Yuu18id@users.noreply.github.com>
2023-03-25 15:48:04 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.purple,
       ),
       debugShowCheckedModeBanner: false,
@@ -33,16 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,140 +33,136 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Todos - 211112007 - Bayu Arma Praja'),
       ),
       body: Center(
-        child: Container(
+        child: SizedBox(
+          width: 440,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    RichText(
-                        text: const TextSpan(children: [
-                      WidgetSpan(child: Icon(Icons.list_alt)),
-                      TextSpan(
-                        text: 'Kegiatan',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ])),
-                    Container(
+                    const Icon(Icons.list_alt),
+                    const Padding(padding: EdgeInsets.only(right: 7)),
+                    const Text(
+                      'Kegiatan',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const Padding(padding: EdgeInsets.only(right: 80)),
+                    SizedBox(
                       width: 230,
                       child: TextFormField(
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
                             labelText: 'Judul Kegiatan'),
                       ),
                     ),
                   ],
                 ),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Row(
-                    children: [
-                      RichText(
-                          text: const TextSpan(children: [
-                        WidgetSpan(child: Icon(Icons.sort)),
-                        TextSpan(
-                          text: 'Keterangan',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ])),
-                    ],
-                  ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                child: Row(
+                  children: const [
+                    Icon(Icons.sort),
+                    Padding(padding: EdgeInsets.only(right: 7)),
+                    Text(
+                      'Keterangan',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(left: 20.0),
-                width: 355,
+                padding: const EdgeInsets.only(left: 50.0, top: 10),
+                width: 415,
                 child: TextFormField(
-                  decoration: const InputDecoration(
-                      contentPadding: EdgeInsets.all(40),
-                      border: OutlineInputBorder(),
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.only(
+                          top: 40, bottom: 40, left: 15, right: 40),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
                       labelText: 'Tambah Keterangan'),
                 ),
               ),
               Container(
+                padding: const EdgeInsets.only(left: 20, top: 20),
                 child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 55),
-                      child: RichText(
-                          text: const TextSpan(children: [
-                        WidgetSpan(child: Icon(Icons.calendar_month)),
-                        TextSpan(
-                          text: 'Tanggal Mulai',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ])),
+                  children: const [
+                    Icon(Icons.calendar_month),
+                    Padding(padding: EdgeInsets.only(right: 7)),
+                    Text(
+                      'Tanggal Mulai',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                    RichText(
-                        text: const TextSpan(children: [
-                      WidgetSpan(child: Icon(Icons.calendar_month_outlined)),
-                      TextSpan(
-                        text: 'Tanggal Selesai',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ])),
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      width: 120,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            labelText: '20-03-2022'),
-                      ),
-                    ),
-                    Container(
-                      width: 120,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            labelText: '20-03-2022'),
-                      ),
+                    Padding(padding: EdgeInsets.only(left: 85)),
+                    Icon(Icons.calendar_month_outlined),
+                    Padding(padding: EdgeInsets.only(right: 7)),
+                    Text(
+                      'Tanggal Selesai',
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
               ),
               Container(
+                padding: const EdgeInsets.only(left: 50, top: 10),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                        width: 180,
+                    SizedBox(
+                      width: 120,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: '20-03-2022',
+                            labelStyle: TextStyle(fontSize: 14)),
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.only(left: 90)),
+                    SizedBox(
+                      width: 120,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: '20-03-2022',
+                            labelStyle: TextStyle(fontSize: 14)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 20, left: 25),
+                child: Row(
+                  children: [
+                    SizedBox(
+                        width: 190,
                         child: OutlinedButton(
-                          child: const Text('Batal'),
+                          style: OutlinedButton.styleFrom(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 18)),
                           onPressed: () {},
+                          child: const Text('Batal',
+                              style: TextStyle(fontWeight: FontWeight.bold)),
                         )),
-                    Padding(padding: EdgeInsets.all(5)),
-                    Container(
-                        width: 180,
+                    const Padding(padding: EdgeInsets.all(5)),
+                    SizedBox(
+                        width: 190,
                         child: ElevatedButton(
-                          child: const Text('Simpan'),
+                          style: ElevatedButton.styleFrom(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 18)),
                           onPressed: () {},
+                          child: const Text(
+                            'Simpan',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
                         )),
                   ],
                 ),
@@ -194,8 +171,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
